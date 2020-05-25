05441fb5530540ff9cef4a5617c2dfdffc9f3457
bicarlsen <carlsen.bri@gmail.com>
2020-05-25 20:53:38 +0200

    Updated version

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,7 @@ author = 'Brian Carlsen'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
