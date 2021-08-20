a97deead0acabaab5febabe33a65e39b42db2bd5
Brian Carlsen <carlsen.bri@gmail.com>
2021-08-20 12:53:21 +0200

    Updated version.

--- source/conf.py
+++ source/conf.py
@@ -24,9 +24,9 @@ copyright = '2021, Brian Carlsen'
 author = 'Brian Carlsen'
 
 # The short X.Y version
-version = '0.3'
+version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -117,7 +117,7 @@ html_js_files = []
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ThotDatadoc'
+htmlhelp_basename = 'ThotDataDoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
